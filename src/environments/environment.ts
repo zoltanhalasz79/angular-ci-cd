7c77f7eed6d513e80b0faa43805b385335f04709
zoltanhalasz79 <zoltan.halasz@gmail.com>
2026-02-14 17:43:07 +0200

    add firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "YOUR_API_KEY",
+        authDomain: "YOUR_https://www.google.com/search?q=PROJECT.firebaseapp.com",
+        projectId: "YOUR_PROJECT_ID",
+        storageBucket: "YOUR_https://www.google.com/search?q=PROJECT.appspot.com",
+        messagingSenderId: "YOUR_SENDER_ID",
+        appId: "YOUR_APP_ID"
+    }
+};
